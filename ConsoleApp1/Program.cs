48685e93bda9e6710b96430e817ea1b1b121028c
GrandheSaiRaghuVamsi <139790743+GrandheSaiRaghuVamsi@users.noreply.github.com>
2025-01-29 12:42:25 +0530

    Array demo

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Enter a mark : ");
+        int n = Convert.ToInt32(Console.ReadLine());
+        
+        if (n % 2 == 0)
+        {
+            Console.WriteLine("Its a even");
+        }
+        else 
+        {
+            Console.WriteLine("Its a ODD");
+        }
+    }
+}
